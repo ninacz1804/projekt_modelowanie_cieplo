be7e2dd6536ea71febe10c2d6f15895278c17804
ninacz1804 <ninaczajkowska1@wp.pl>
2026-02-02 19:46:00 +0100

    Create config.py

---
+++ pipeline/config.py
@@ -0,0 +1,16 @@
+nx = 50
+ny = 50
+szerokosc = 3.0
+h = szerokosc / (nx - 1)
+T_startowa = 15.0
+T_okna = 5.0
+T_grzejnika = 40.0
+T_cel = 21.0
+
+czujnik_x = 25
+czujnik_y = 25
+idx_czujnika = czujnik_y * nx + czujnik_x
+
+dt = 20.0
+kroki = 5000
+mnoznik = 100.0
