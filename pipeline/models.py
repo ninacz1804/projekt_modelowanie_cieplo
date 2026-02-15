3cd8e49dfb3bb9c30f4431c6aba73e27d213b5ff
ninacz1804 <ninaczajkowska1@wp.pl>
2026-02-15 14:23:37 +0100

    Update models.py

--- pipeline/models.py
+++ pipeline/models.py
@@ -64,10 +64,10 @@ def termostat(u_start, A_inv_on, A_inv_off, T_grzejnika, T_cel, idx_czujnika, kr
             
     return u
 
-def symulacja_pokoju(x_grz, y_grz, nx, ny, alfa, dt, h, T_start, T_okna, T_grzejnika, T_cel, idx_czujnika, kroki):
+def symulacja_pokoju(x_grz, y_grz, szer_grz, wys_grz, nx, ny, alfa, dt, h, T_start, T_okna, T_grzejnika, T_cel, idx_czujnika, kroki):
     pokoj = np.ones((ny, nx)) * T_start
     pokoj[0, 15:35] = T_okna
-    pokoj[y_grz : y_grz + 4, x_grz : x_grz + 10] = T_grzejnika
+    pokoj[y_grz : y_grz + wys_grz, x_grz : x_grz + szer_grz] = T_grzejnika
     
     u_vector = pokoj.flatten()
 
