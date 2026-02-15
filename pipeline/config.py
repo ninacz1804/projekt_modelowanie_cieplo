bd895383e399af01e9f4f910f10010e5d015b6ae
ninacz1804 <ninaczajkowska1@wp.pl>
2026-02-15 16:36:39 +0100

    Update config.py

--- pipeline/config.py
+++ pipeline/config.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 nx = 50
 ny = 50
 szerokosc = 3.0
@@ -12,7 +14,7 @@ idx_czujnika = czujnik_y * nx + czujnik_x
 
 
 dt = 20.0
-kroki = 5000
+kroki = 10000
 mnoznik = 100.0
 
 nx_3 = 90
@@ -29,3 +31,18 @@ idx_czujnika_sasiad_2 = czujnik_y_sasiad_2 * nx_3 + czujnik_x_sasiad_2
 
 szerokosc = 10
 wysokosc = 4
+
+m_s1 = np.zeros((ny_3, nx_3), dtype=bool)
+m_s1[1:5, 10:20] = True
+maska_s1 = m_s1.flatten()
+
+m_ja = np.zeros((ny_3, nx_3), dtype=bool)
+m_ja[1:5, 40:50] = True
+maska_ja = m_ja.flatten()
+
+m_s2 = np.zeros((ny_3, nx_3), dtype=bool)
+m_s2[1:5, 70:80] = True
+maska_s2 = m_s2.flatten()
+
+lista_masek = [maska_s1, maska_ja, maska_s2]
+idx_czujnikow = [idx_czujnika_sasiad_1, idx_czujnika, idx_czujnika_sasiad_2]
